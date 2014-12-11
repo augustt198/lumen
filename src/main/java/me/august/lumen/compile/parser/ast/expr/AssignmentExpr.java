402692e59cbac750fbdc2d7e73d1793cec7ef897
August <augustt198@gmail.com>
2014-12-11 17:15:23 -0500

    Add assignment expression bytecode generation
    Only reference types currently supported (ASTORE)

--- src/main/java/me/august/lumen/compile/parser/ast/expr/AssignmentExpr.java
+++ src/main/java/me/august/lumen/compile/parser/ast/expr/AssignmentExpr.java
@@ -1,11 +1,21 @@
 package me.august.lumen.compile.parser.ast.expr;
 
+import me.august.lumen.compile.codegen.BuildContext;
 import me.august.lumen.compile.scanner.Op;
+import org.objectweb.asm.MethodVisitor;
 
 public class AssignmentExpr extends BinaryExpression {
 
-    public AssignmentExpr(IdentExpr right, Expression left) {
-        super(right, left, Op.ASSIGN);
+    // BinaryExpression only defines left as general expression
+    IdentExpr left;
+
+    public AssignmentExpr(IdentExpr left, Expression right) {
+        super(left, right, Op.ASSIGN);
+        this.left = left;
     }
 
+    @Override
+    public void generate(MethodVisitor visitor, BuildContext context) {
+        left.getRef().generateSetCode(visitor, (m) -> right.generate(visitor, context));
+    }
 }
