37b97dcbcef1cb9e9ae6964bdb9c1a38ae4e33f1
August <augustt198@gmail.com>
2015-01-02 17:06:40 -0500

    Better branching

--- src/main/java/me/august/lumen/compile/parser/ast/stmt/IfStmt.java
+++ src/main/java/me/august/lumen/compile/parser/ast/stmt/IfStmt.java
@@ -1,18 +1,38 @@
 package me.august.lumen.compile.parser.ast.stmt;
 
+import jdk.internal.org.objectweb.asm.Opcodes;
 import me.august.lumen.compile.analyze.ASTVisitor;
 import me.august.lumen.compile.analyze.VisitorConsumer;
 import me.august.lumen.compile.codegen.Branch;
 import me.august.lumen.compile.codegen.BuildContext;
 import me.august.lumen.compile.codegen.Conditional;
+import me.august.lumen.compile.codegen.MethodCodeGen;
 import me.august.lumen.compile.parser.ast.CodeBlock;
 import me.august.lumen.compile.parser.ast.expr.Expression;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Type;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
 
 public class IfStmt implements CodeBlock, VisitorConsumer {
+
+    private static Branch toBranch(Expression expr, MethodCodeGen ifBranch, MethodCodeGen elseBranch) {
+        if (expr instanceof Conditional) {
+            return ((Conditional) expr).branch(ifBranch, elseBranch);
+        } else if (expr.expressionType().getSort() == Type.BOOLEAN) {
+            Label label = new Label();
+            MethodCodeGen cond = (m, c) -> {
+                expr.generate(m, c);
+                m.visitJumpInsn(Opcodes.IFEQ, label);
+            };
+            return new Branch(cond, label, ifBranch, elseBranch);
+        } else {
+            throw new IllegalArgumentException("Incompatible types.");
+        }
+    }
 
     private Expression condition;
     private Body trueBody;
@@ -32,12 +52,12 @@ public class IfStmt implements CodeBlock, VisitorConsumer {
 
     @Override
     public void generate(MethodVisitor visitor, BuildContext context) {
-        Branch branch = ((Conditional) condition).branch(trueBody, elseBody);
+        Branch branch = toBranch(condition, trueBody, elseBody);
 
         if (elseIfs != null) {
             for (ElseIf elseIf : elseIfs) {
                 branch.setElseBranch(
-                    ((Conditional) elseIf.condition).branch(elseIf.body, branch.getElseBranch())
+                    toBranch(elseIf.condition, elseIf.body, branch.getElseBranch())
                 );
             }
         }
